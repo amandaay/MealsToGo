28e5cdb5fdcedb2915fa59350448763a0ad05b82
amandaay <amandaay.aysma@gmail.com>
2024-06-20 17:46:04 +0800

    updated with styled-components

--- App.js
+++ App.js
@@ -1,37 +1,12 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Searchbar } from 'react-native-paper';
-import { StatusBar, StyleSheet, SafeAreaView, Text, View } from 'react-native';
+import { RestaurantScreen } from './src/features/restaurants/screens/restaurant.screen';
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Searchbar />
-        </View>
-        <View style={styles.list}>
-          <Text>list</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantScreen />
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // currentHeight fixes issue with Android phones
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-    backgroundColor: 'green',
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: 'blue',
-  },
-});
