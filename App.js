e016be3095fd13a5d82be4197809e56b10910e34
amandaay <amandaay.aysma@gmail.com>
2024-06-25 19:59:13 +0800

    added mock data to list

--- App.js
+++ App.js
@@ -2,18 +2,20 @@ import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Text } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+
 import {
   useFonts as useOswald,
   Oswald_400Regular,
 } from '@expo-google-fonts/oswald';
 import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
+
 import { theme } from './src/infrastructure/theme';
 import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { SafeArea } from './src/components/utils/safe-area.component';
-import { Ionicons } from '@expo/vector-icons';
-import { restaurantsRequest } from './src/services/restaurants/restaurants.service';
+import { SafeArea } from './src/components/utility/safe-area.component';
+import { RestaurantsContextProvider } from './src/services/restaurants/restaurants.context';
 
 const Tab = createBottomTabNavigator();
 
@@ -23,24 +25,31 @@ const TAB_ICON = {
   Settings: 'settings',
 };
 
-const createScreenOptions = ({ route }) => ({
-  tabBarIcon: ({ focused, color, size }) => {
-    const iconName = TAB_ICON[route.name];
-    return <Ionicons name={focused ? `${iconName}` : `${iconName}-outline`} size={size} color={color} />;
-  },
-});
-
 const Settings = () => (
   <SafeArea>
     <Text>Settings</Text>
   </SafeArea>
 );
-
 const Map = () => (
   <SafeArea>
     <Text>Map</Text>
   </SafeArea>
 );
+
+const createScreenOptions = ({ route }) => ({
+  tabBarIcon: ({ focused, color, size }) => {
+    const iconName = TAB_ICON[route.name];
+    return (
+      <Ionicons
+        name={focused ? iconName : `${iconName}-outline`}
+        size={size}
+        color={color}
+      />
+    );
+  },
+  tabBarActiveTintColor: 'tomato',
+  tabBarInactiveTintColor: 'gray',
+});
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -58,19 +67,15 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <NavigationContainer>
-          <Tab.Navigator
-            screenOptions={createScreenOptions}
-            tabBarOptions={{
-              activeTintColor: 'tomato',
-              inactiveTintColor: 'gray',
-            }}
-          >
-            <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
-            <Tab.Screen name="Map" component={Map} />
-            <Tab.Screen name="Settings" component={Settings} />
-          </Tab.Navigator>
-        </NavigationContainer>
+        <RestaurantsContextProvider>
+          <NavigationContainer>
+            <Tab.Navigator screenOptions={createScreenOptions}>
+              <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+              <Tab.Screen name="Map" component={Map} />
+              <Tab.Screen name="Settings" component={Settings} />
+            </Tab.Navigator>
+          </NavigationContainer>
+        </RestaurantsContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
