d8efcba2b750a46e60bb83fb351689fe11dcafee
amandaay <amandaay.aysma@gmail.com>
2024-07-02 22:56:01 +0800

    authenticate

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ThemeProvider } from 'styled-components/native';
 import { Navigation } from './src/infrastructure/navigation/index';
 import {
@@ -11,8 +11,43 @@ import { theme } from './src/infrastructure/theme';
 import { RestaurantsContextProvider } from './src/services/restaurants/restaurants.context';
 import { LocationContextProvider } from './src/services/location/location.context';
 import { FavoritesContextProvider } from './src/services/favorites/favorites.context';
+import { initializeApp } from 'firebase/app';
+// import { AuthenticationContextProvider } from './src/services/authentication/authentication.context';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: 'AIzaSyDDbZaLUx0oLZTJoSzHg71C9zWXNVZFEL8',
+  authDomain: 'mealstogo-40434.firebaseapp.com',
+  projectId: 'mealstogo-40434',
+  storageBucket: 'mealstogo-40434.appspot.com',
+  messagingSenderId: '122276154260',
+  appId: '1:122276154260:web:d906b71796cd8e5b801d81',
+};
+
+initializeApp(firebaseConfig);
 
 export default function App() {
+  const auth = getAuth();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      signInWithEmailAndPassword(auth, 'a@a.com', 'test123')
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          setIsAuthenticated(true);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          console.log('error code', errorCode);
+          const errorMessage = error.message;
+          console.log('error message', errorMessage);
+        });
+    }, 2000);
+  }, []);
+
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
   });
@@ -28,6 +63,7 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
+        {/* <AuthenticationContextProvider> */}
         <FavoritesContextProvider>
           <LocationContextProvider>
             <RestaurantsContextProvider>
@@ -35,6 +71,7 @@ export default function App() {
             </RestaurantsContextProvider>
           </LocationContextProvider>
         </FavoritesContextProvider>
+        {/* </AuthenticationContextProvider> */}
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
